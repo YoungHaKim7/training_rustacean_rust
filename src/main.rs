46eddc1f69a03776b4fed76e68b49a3679f27adf
YoungHa Kim <ytok1108@aol.com>
2022-02-05 21:39:45 +0900

    for__rev() result: 3! 2! 1! launching

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let a = [10, 20, 30, 40, 50];
-    for element in a.iter() {
-        println!("요소의 값: {element}");
+    for number in (1..4).rev() {
+        println!("{number}!");
     }
+    println!("발사!!!");
 }
