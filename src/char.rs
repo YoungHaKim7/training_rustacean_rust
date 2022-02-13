f2984962dac6a2c8fea63012d58a0fd50674d6d8
YoungHa Kim <ytok1108@aol.com>
2022-02-13 18:48:11 +0900

    Rust calculator

---
+++ src/char.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let first_letter = '|';
+    let overflowing:u8 = 255;
+    println! {"first_letter:{}", first_letter as u8};
+    println! {"overflowing : {}",overflowing};
+    
+
+}
