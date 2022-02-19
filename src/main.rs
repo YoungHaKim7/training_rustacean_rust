76a109e88d1874e1cb4d684bdc5318dec8c0873c
YoungHa Kim <ytok1108@aol.com>
2022-02-19 17:24:35 +0900

    C++20

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,7 @@
-use std::env::{args, Args};
-
 fn main() {
-    let mut args: Args = args();
+    let my_number: f32 = 1.12345678;
+    let my_number64: f64 = 1.123456789012345678901234567890;
 
-    let first = args.nth(1).unwrap();
-    let operator = args.nth(0).unwrap().chars().next().unwrap();
-    let second = args.nth(0).unwrap();
-
-    let first_number = first.parse::<f32>().unwrap();
-    let second_number = second.parse::<f32>().unwrap();
-    let result = operate(operator, first_number, second_number);
-
-    println!(
-        "{:?}",
-        output(first_number, operator, second_number, result)
-    );
-}
-
-fn operate(operator: char, first_number: f32, second_number: f32) -> f32 {
-    match operator {
-        '+' => first_number + second_number,
-        '-' => first_number - second_number,
-        '/' => first_number / second_number,
-        '*' | 'x' | 'X' => first_number * second_number,
-        _ => panic!("Invalid operator used."),
-    }
-}
-
-fn output(first_number: f32, operator: char, second_number: f32, result: f32) -> String {
-    format!(
-        "{} {} {} = {}",
-        first_number, operator, second_number, result
-    )
+    println!("float __32bit : {}", my_number);
+    println!("float __64bit : {}", my_number64);
 }
