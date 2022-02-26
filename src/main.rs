3d730416192a857025aea1d95205110b6d05bf5e
YoungHa Kim <ytok1108@aol.com>
2022-02-26 16:34:07 +0900

    Methods with More Parameters

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,36 @@
-// Adding Useful Functionality with Derived Traits
+// Methods with More Parameters
 #[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
+
 impl Rectangle {
-    fn width(&self) -> bool {
-        self.width > 0
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
 fn main() {
     let rect1 = Rectangle {
-        width:30,
-        height: 50, 
+        width: 30,
+        height: 50,
     };
 
-    if rect1.width() {
-        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
 
-    }
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {} ", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {} ", rect2.can_hold(&rect3));
 }
