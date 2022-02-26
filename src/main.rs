97229839c13a6b6ec92ff820e317b7db277850e4
YoungHa Kim <ytok1108@aol.com>
2022-02-26 15:12:45 +0900

    Defining and Instantiating Structs

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,17 @@
-use std::io::{stdin, stdout, Write};
+// Defining and Instantiating Structs
 
-fn read(input: &mut String) {
-    stdout().flush().expect("failed to flush");
-    stdin().read_line(input).expect("failed to read");
+struct User {
+    active: bool, 
+    username: String,
+    email: String,
+    sign_in_count: u64
 }
 
-fn main() {
-    println!("Welcome to engineer man's calculator! ");
-    println!("--------------------------");
-
-    let mut num1 = String::new();
-    let mut num2 = String::new();
-    let mut operator = String::new();
-
-    println!("What is the first number? :  ");
-    read(&mut num1);
-
-    println!("What is the sencond number? :  ");
-    read(&mut num2);
-
-    println!("What operation would you like to do? [ + - * / ]:  ");
-    read(&mut operator);
-
-    let num1: f32 = num1.trim().parse().unwrap();
-    let num2: f32 = num2.trim().parse().unwrap();
-    let operator: char = operator.trim().chars().next().unwrap();
-
-    let operators = String::from(" + - * / ");
-
-    if !operators.contains(operator) {
-        println!("unknown operator");
-        return;
-    }
-
-    let result = match operator {
-        '+' => num1 + num2,
-        '-' => num1 - num2,
-        '*' => num1 * num2,
-        '/' => num1 / num2,
-        _ => panic!("error inperator"),
+fn main () {
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
     };
-
-    println!("the result of {} {} {} = {}", num1, operator, num2, result);
 }
