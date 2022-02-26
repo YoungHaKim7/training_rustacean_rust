009054d9429e85b708f47820fd9ca028eae47514
YoungHa Kim <ytok1108@aol.com>
2022-02-26 15:34:51 +0900

    unit-like struct

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
-// tuple structs
 
-struct Color(i32,i32,i32);
-struct Point(i32,i32,i32);
+struct User {
+    active: &str, 
+    email: &str,
+    sign_in_count: u64
+    active: bool
+}
 
-let black = Color(0, 0, 0);
-let origin = Point(0,0,0);
+fn main () {
+    let user1 = User {
+        email: "someone@example.com",
+        username: "someusername123",
+        active: true,
+        sign_in_count: 1,
+    };
+}
