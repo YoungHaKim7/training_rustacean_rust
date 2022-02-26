05e4939fa53492c63d896bad459f02782b4d72ae
YoungHa Kim <ytok1108@aol.com>
2022-02-26 16:06:30 +0900

    Adding Useful Functionality with Derived Traits

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-// Refactoring with structs: Adding More Meaning
-
+// Adding Useful Functionality with Derived Traits
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -10,9 +10,5 @@ fn main() {
         height: 50, 
     };
 
-    println!("The area of the rectangle is {} square pixels", area(&rect1));
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("rect1 is {:?}",&rect1);
 }
