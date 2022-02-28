d4c278fb745bb4ab13215be438536e2172fa1318
YoungHa Kim <ytok1108@aol.com>
2022-02-28 22:46:42 +0900

    Storing the data and __IpAddrKind__ variant of an IP address using a __struct

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,31 @@
-// Defining an Enum
+// Storing the data and "IpAddrKind" variant of an IP address using a "struct"
+use std::fmt::Debug;
+
 #[derive(Debug)]
 enum IpAddrKind {
     V4,
     V6,
 }
 
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
 fn route(ip_kind: IpAddrKind) {}
 
 fn main() {
-    // We can call this function with either variant:
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
 
-    println!("{:?}", route(IpAddrKind::V4));
-    println!("{:?}", route(IpAddrKind::V6));
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
+
+    println!("{:?}", home.address);
+    println!("{:?}", loopback.address);
 }
 
