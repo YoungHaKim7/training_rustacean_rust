5faa86eff2d5c7dc22d75ea83e950633826eab85
YoungHa Kim <ytok1108@aol.com>
2022-02-28 22:35:17 +0900

    Defining an Enum

--- src/char.rs
+++ src/char.rs
@@ -3,6 +3,6 @@ fn main() {
     let overflowing:u8 = 255;
     println! {"first_letter:{}", first_letter as u8};
     println! {"overflowing : {}",overflowing};
-    
+    println!{} 
 
 }
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,16 @@
-
-// Associated Functions
+// Defining an Enum
 #[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-}
-
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-    
-    fn square(size: u32) -> Rectangle {
-        Rectangle { width: size , height: size }
-    }
+enum IpAddrKind {
+    V4,
+    V6,
 }
 
 fn main() {
-    let rect1 = Rectangle {
-        width: 30,
-        height: 50,
-    };
+    // We can create instances of each of the tow variants of "IpAddrKind" 
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
 
-    let rect2 = Rectangle {
-        width: 10,
-        height: 40,
-    };
-
-    let rect3 = Rectangle {
-        width: 60,
-        height: 45,
-    };
-
-    let sq = Rectangle::square(3);
-
-    println!("Can rect1 hold rect2? {} ", rect1.can_hold(&rect2));
-    println!("Can rect1 hold rect3? {} ", rect2.can_hold(&rect3));
-    println!("Rectangle__Associated Functions {:#?}", sq);
+    println!("{four:?}");
+    println!("{six:?}");
 }
+
