5a998274026054646076568ceb15e14f00e7f8a2
YoungHa Kim <ytok1108@aol.com>
2022-03-01 21:45:10 +0900

    Catch-all Patterns and the _ Placeholder

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,22 @@
-// Matching with Option<T>
+// Catch-all Patterns and the _ Placeholder
 //
-fn plus_one(x: Option<i32>) -> Option<i32> {
-    match x {
-        None => None,
-        Some(i) => Some(i + 1),
-    }
+fn add_fancy_hat() {
+    println!("add_fancy_hat!!!function!!")
+}
+fn remove_fancy_hat() {
+    println!("remove_fancy_hat!!!function!!")
+}
+fn move_player(num_spaces: u8) {
+    println!("move_player__!!!function!!")
 }
 
 fn main() {
-    let five = Some(5);
-    let six = plus_one(five);
-    let none = plus_one(None);
 
-    println!("{five:?}");
-    println!("{six:?}");
-    println!("{none:?}");
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        other => move_player(other),
+    }
+
 }
