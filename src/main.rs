8dc98e9d1680feb3874894dbec114729f6e26971
YoungHa Kim <ytok1108@aol.com>
2022-03-01 21:54:18 +0900

    Concise Control Flow with __if let__

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,12 @@
-// we can express that by using the unit value(the empty tuple type we mentioned in "The Tuple Type" section) as the code that goes with the "_" arm:
+// Concise Control Flow with "if let"
 //
-fn add_fancy_hat() {
-    println!("add_fancy_hat!!!function!!")
-}
-fn remove_fancy_hat() {
-    println!("remove_fancy_hat!!!function!!")
-}
-
 fn main() {
 
-    let dice_roll = 9;
-    match dice_roll {
-        3 => add_fancy_hat(),
-        7 => remove_fancy_hat(),
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("The maximum is configured to be {} ", max),
         _ => (),
     }
 
+    println!("{config_max:?}")
 }
