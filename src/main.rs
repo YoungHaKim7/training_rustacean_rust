eec80cbab91603d34aabf28a41c4a2714341fff2
YoungHa Kim <ytok1108@aol.com>
2022-03-01 21:37:24 +0900

    A function that uses a __match__ expression on an __Option<i32>

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,18 @@
-// We can add this information to out "enum" by changing the "Quarter" variant to include a
-// "UsState" value stored inside it, which we've done here
-#[derive(Debug)]
-enum UsState {
-    Alabama,
-    Alaska,
-}
-
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UsState),
-}
-
-fn value_in_cents(coin: Coin) -> u8 {
-    match coin {
-        Coin::Penny => {
-            println!("Lucky penny!");
-            1
-        }
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State quarter from {:?}!", state);
-            25
-        }
+// Matching with Option<T>
+//
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
 }
 
 fn main() {
-    value_in_cents(Coin::Quarter(UsState::Alaska));
-} 
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+
+    println!("{five:?}");
+    println!("{six:?}");
+    println!("{none:?}");
+}
