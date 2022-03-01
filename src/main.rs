7673a9fc335f5e9f4f7b8ac2fcc068ddb95c402c
YoungHa Kim <ytok1108@aol.com>
2022-03-01 21:50:49 +0900

    we can express that by using the unit value(the empty tuple type we mentioned in __The Tuple Type__ section) as the code that goes with the ...._.... arm:Catch-all Patterns and the _ Placeholder

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-// Catch-all Patterns and the _ Placeholder
+// we can express that by using the unit value(the empty tuple type we mentioned in "The Tuple Type" section) as the code that goes with the "_" arm:
 //
 fn add_fancy_hat() {
     println!("add_fancy_hat!!!function!!")
 }
 fn remove_fancy_hat() {
     println!("remove_fancy_hat!!!function!!")
-}
-fn move_player(num_spaces: u8) {
-    println!("move_player__!!!function!!")
 }
 
 fn main() {
@@ -16,7 +13,7 @@ fn main() {
     match dice_roll {
         3 => add_fancy_hat(),
         7 => remove_fancy_hat(),
-        other => move_player(other),
+        _ => (),
     }
 
 }
