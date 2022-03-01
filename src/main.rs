75616e633550571a81f31f82c9939a87719089e1
YoungHa Kim <ytok1108@aol.com>
2022-03-01 21:32:32 +0900

    we can then use that binding in the ___println___ expression, thus getting the inner state value out of the ___Coin___ eunum variant for __Quarter__

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
-// An enum and a "match" expression that has the variants of the enum as its pattern
+// We can add this information to out "enum" by changing the "Quarter" variant to include a
+// "UsState" value stored inside it, which we've done here
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
 
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -15,10 +21,13 @@ fn value_in_cents(coin: Coin) -> u8 {
         }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
 
 fn main() {
-    value_in_cents(Coin::Penny);
+    value_in_cents(Coin::Quarter(UsState::Alaska));
 } 
