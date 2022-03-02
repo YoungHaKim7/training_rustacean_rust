fe567c526aa732752f0b04fe91b7f54712a5f223
YoungHa Kim <ytok1108@aol.com>
2022-03-02 22:23:30 +0900

    Compiler errors from building the code:

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,6 @@
-// This enum is "Option<T>", and it is defined by the standard library as follows:
-#[derive(Debug)]
-enum Option<T> {
-    None,
-    Some(T),
-}
-
-fn main() {
-    let some_number = Option::Some(5);
-    let some_string = Option::Some("a string".to_string());
-
-    let absent_number: Option<i32> = Option::None;
-
-    println!("{some_number:?}");
-    println!("{some_string:?}");
-    println!("{absent_number:?}");
-}
-
+// Module system
+//
+// Packages
+// Crates
+// Modules and use
+// Paths
