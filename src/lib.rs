b18ea3fb06eecf36f7dd6c8b68cf99991eb47ca6
YoungHa Kim <ytok1108@aol.com>
2022-03-02 22:40:57 +0900

    Bringing a module into scope with __use__

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Calling a function using a relative path starting with "super"
+// A struct with some public fields and some private fields
 mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {}
@@ -13,23 +13,51 @@ mod front_of_house {
 
         fn take_payment() {}
     }
-}
-
-pub fn eat_at_restaurant() {
-    // Absolute path
-    crate::front_of_house::hosting::add_to_waitlist();
-
-    // Relative path
-    front_of_house::hosting::add_to_waitlist();
+    
 }
 
 fn serve_order() {}
 
 mod back_of_house {
-    fn fix_incorrect_order() {
-        cook_order();
-        super::serve_order();
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
     }
 
-    fn cook_order() {}
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please ", meal.toast);
+
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // meal.seasonal_fruit = String::from("blueberries");
+
+    // Designating an enum as public makes all its variants public
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
