1cb2bd3b15243822f2dd1ef8027d9edcb5b573b2
YoungHa Kim <ytok1108@aol.com>
2022-03-03 23:00:58 +0900

    Dropping a Vector Drops Its Elements

--- src/front_of_house.rs
+++ src/front_of_house.rs
@@ -1,3 +1,3 @@
-pub mod hosting;
+// pub mod hosting;
 
-pub mod hosting {}
+// pub mod hosting {}
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,16 @@ fn main () {
     v.push(6);
     v.push(7);
     v.push(8);
+    {
+        let v = vec![1,2,3,4];
+
+        println!("Vector v __ scope in : {v:?}")
+
+        // let z = vec![5,6,7,8,9,10]; // < z goes out of scope and is freed here___drop "z"
+        // do stuff with v
+    } // <-- v goes out of scope and is freed here
 
     println!("Vector print: {v:?}");
+//    println!("Vector print: {z:?}");
 }
 
