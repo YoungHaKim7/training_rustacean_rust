ddbf6389a29c53e21ed3c9e26f4094c27373b82a
YoungHa Kim <ytok1108@aol.com>
2022-03-03 23:04:15 +0900

    Using indexing syntax or the get method to access an item ina vector

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,11 @@
-// Creating a new Vector
 fn main () {
-    let mut v = Vec::new();
+    let v = vec![1,2,3,4,5];
 
-    v.push(5);
-    v.push(6);
-    v.push(7);
-    v.push(8);
-    {
-        let v = vec![1,2,3,4];
+    let third: &i32 = &v[2];
+    println!("The third element is {}", third);
 
-        println!("Vector v __ scope in : {v:?}")
-
-        // let z = vec![5,6,7,8,9,10]; // < z goes out of scope and is freed here___drop "z"
-        // do stuff with v
-    } // <-- v goes out of scope and is freed here
-
-    println!("Vector print: {v:?}");
-//    println!("Vector print: {z:?}");
+    match v.get(2) {
+        Some(third) => println!("The third element is {}", third),
+        None => println!("There is no third element."),
+    }
 }
-
