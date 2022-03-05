92ba46b4680f25c1218ceec6ecbbb46fb7a0d66f
YoungHa Kim <ytok1108@aol.com>
2022-03-05 16:18:27 +0900

    Yahoo_Finance_test

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
-// Updating a Value Based on the Old Value
-use std::collections::HashMap;
+use tokio_test;
+use yahoo_finance_api as yahoo;
 
 fn main() {
-    let text = "Hello world wonderful world";
+    let provider = yahoo::YahooConnector::new();
+    let resp = tokio_test::block_on(provider.search_ticker("Apple")).unwrap();
 
-    let mut map = HashMap::new();
-
-    for word in text.split_whitespace() {
-        let count = map.entry(word).or_insert(0);
-        *count += 1;
+    let mut apple_found = false;
+    println!("All tickers found while searching for 'Apple':");
+    for item in resp.quotes {
+        println!("{}", item.symbol)
     }
-
-    println!("{:?}", map);
-
 }
