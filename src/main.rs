cab79b455ab8880ef80aae5c991c4c1bfce3f427
YoungHa Kim <ytok1108@aol.com>
2022-03-06 00:23:21 +0900

    Using __fs::read_to_string__ instead of opening and then reading the file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,11 @@
 // A shorcut for Propagating Errors: the ? Operator
-// Chaining method call after the "?" operator
+// Using "fs::read_to_string" instead of opening and then reading the file
 
-use std::fs::File; 
-use std::io::{self, Read};
+use std::fs; 
+use std::io;
 
 fn read_username_from_file() -> Result<String, io::Error> {
-    let mut s = String::new();
-
-    File::open("hello.txt")?.read_to_string(&mut s)?;
-
-    Ok(s)
+    fs::read_to_string("hello.txt")
 }
 
 fn main() {}
