b54082b5537ee29a4bb2b348c754d0ac093ca1ad
YoungHa Kim <ytok1108@aol.com>
2022-03-06 00:30:33 +0900

    Using the __?__ Operator on an __Option<T>__ value

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 // A shorcut for Propagating Errors: the ? Operator
 // Where the "?" Operator Can Be Used
+// Using the "?" Operator on an "Option<T>" value
 
 use std::fs::File; 
 
-fn main() {
-    let f = File::open("hello.txt")?;
+fn last_char_of_first_line(text: &str) -> Option<char> {
+    text.lines().next()?.chars().last()
 }
+
+fn main() {}
