e109d10faf107935c9a762fd5324ba417cbbac66
YoungHa Kim <ytok1108@aol.com>
2022-03-06 20:38:40 +0900

    Defining a trait

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,10 @@
-// The generic "Option<T>" is replaced with the specific definitios created by the compiler:
+// Defining a trait
 //
+// Traits are similar to a featue often called interfaces in other languages, although with some differences.
+// A "Summary" trait that consists of the behavior provided by a "summarize" method
 
-#[derive(Debug)]
-enum Option_i32 {
-    Some(i32),
-    None,
+pub trait Summary {
+    fn summary(&self) -> String;
 }
 
-#[derive(Debug)]
-enum Option_f64 {
-    Some(f64),
-    None,
-}
-
-fn main() {
-    let integer = Option_i32::Some(5);
-    let float = Option_f64::Some(5.0);
-
-    println!("integer Option_i32 is : {:?}", integer);
-    println!("float Option_f64 is : {:?}", float);
-}
+fn main() {}
