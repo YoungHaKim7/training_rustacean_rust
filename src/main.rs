a4c0c0030ab2d26509a4cebbb825863f5d5e13fe
YoungHa Kim <ytok1108@aol.com>
2022-03-06 20:15:22 +0900

    Two functions that differ only in their names and the types in their signature

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,36 @@
-// Generic Types, Traits.
-// Code to find the largest number in a list of numbers
-//
+// Generic Data Types.
+// Two functions that differ only in their names and the types in their signatures
+
+fn largest(list: &[i32]) -> i32 {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
+
+fn largest_char(list: &[char]) -> char {
+    let mut largest = list[0];
+
+    for &item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+    largest
+}
 
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65, 90];
 
-    let mut largest = number_list[0];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
 
-    for number in number_list {
-        if number > largest {
-            largest = number;
-        }
-    }
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest_char(&char_list);
+    println!("The largest char is {}", result);
 }
