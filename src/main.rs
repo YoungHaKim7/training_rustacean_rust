18abd85635354c857bee27fa89b96247e89d6883
YoungHa Kim <ytok1108@aol.com>
2022-03-06 20:34:05 +0900

    The generic Option<T> is replaced with the specific definitios created by the compiler

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
-// A method that uses different generic types from its struct's definition
-struct Point<X1, Y1> {
-    x: X1,
-    y: Y1,
+// The generic "Option<T>" is replaced with the specific definitios created by the compiler:
+//
+
+#[derive(Debug)]
+enum Option_i32 {
+    Some(i32),
+    None,
 }
 
-
-impl<X1, Y1> Point<X1, Y1> {
-    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
-        Point {
-            x: self.x,
-            y: other.y
-        }
-    }
+#[derive(Debug)]
+enum Option_f64 {
+    Some(f64),
+    None,
 }
 
 fn main() {
-    let p1 = Point { x:5, y: 10.4 };
-    let p2 = Point { x: "Hello", y: 'c'};
+    let integer = Option_i32::Some(5);
+    let float = Option_f64::Some(5.0);
 
-    let p3 = p1.mixup(p2);
-
-    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+    println!("integer Option_i32 is : {:?}", integer);
+    println!("float Option_f64 is : {:?}", float);
 }
