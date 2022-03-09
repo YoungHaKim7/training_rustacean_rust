a6ed657aa28d75097d064be8fda85fe2b6121f7e
YoungHa Kim <ytok1108@aol.com>
2022-03-09 21:13:00 +0900

    Easy_rust_Eng.063__Trait_Deep Dive

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,20 @@ trait Canine {
     fn bark(&self) {
         println!("Woof woof");
     }
-    fn run(&self) {
-        println!("The dog is running");
-    }
+
+    //    fn run(&self) {
+    //        println!("The dog is running");
+    //    }
+    //
+    fn run(&self);
 }
 
 impl Canine for Animal {
-    fn run(&self) {
-        let my_number = 5;
-        println!("Dog number {} is running", my_number);
-    }
+// fn run(&self) {
+//     let my_number = 5;
+//     println!("Dog number {} is running", my_number);
+//     my_number
+// }
 }
 
 fn main() {
