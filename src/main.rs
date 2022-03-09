8e1fbb6ffc9d2a7c36c72885827ff18280109c34
YoungHa Kim <ytok1108@aol.com>
2022-03-10 05:17:41 +0900

    Easy_rust_Eng.064__Trait_part2__003_result: Reggie Mantle is a Cat { name: _Reggie Mantle_, age: 4 }

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-struct Numbers {
-    number1: u8,
-    number2: u8
+#[derive(Debug)]
+struct Cat {
+    name: String,
+    age: u8
 }
 
-fn main() {
-    let my_number = Numbers {
-        number1: 8,
-        number2: 17,
+fn main () {
+    let mr_mantle = Cat {
+        name: "Reggie Mantle".to_string(),
+        age: 4
     };
+    println!("Reggie Mantle is a {:?}", mr_mantle);
 }
