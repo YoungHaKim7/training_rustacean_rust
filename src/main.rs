8632594ccc40720aac03bfa38b0ebfeee00ad823
YoungHa Kim <ytok1108@aol.com>
2022-03-10 05:21:25 +0900

    Easy_rust_Eng.064__Trait_part3__004_result: Reggie Mantle is a Cat { name: _Reggie Mantle_, age: 4 }

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 #[derive(Debug)]
 struct Cat {
     name: String,
-    age: u8
+    age: u8,
 }
 
-fn main () {
+impl std::fmt::Display for Cat {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.name, self.age)
+    }
+}
+
+fn main() {
     let mr_mantle = Cat {
         name: "Reggie Mantle".to_string(),
-        age: 4
+        age: 4,
     };
     println!("Reggie Mantle is a {:?}", mr_mantle);
 }
