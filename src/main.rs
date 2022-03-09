9cb15eff718ff3e0f172bfbeaae15f1fd225568d
YoungHa Kim <ytok1108@aol.com>
2022-03-10 05:45:34 +0900

    Easy_rust_Eng.064__Trait_part4__003__result: You attack with your sword. Your opponent has 30 health left. You attack with your bow. Your opponent has 20 health left

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 struct Monster {
-    health: i32
+    health: i32,
 }
 
 struct Wizard {}
@@ -8,9 +8,55 @@ struct Ranger {}
 trait FightClose {
     fn attack_with_sword(&self, opponent: &mut Monster) {
         opponent.health -= 10;
-        println!("You attack with your sword. Your opponent has {} health left.", opponent.health);
+        println!(
+            "You attack with your sword. Your opponent has {} health left.",
+            opponent.health
+        );
+    }
+
+    fn attack_with_hand(&self, opponent: &mut Monster) {
+        opponent.health -= 2;
+        println!(
+            "You attack with your opponent now has {} health left",
+            opponent.health
+        );
     }
 }
 
+impl FightClose for Wizard {}
+impl FightClose for Ranger {}
 
-fn main () {}
+trait FightFromDistance {
+    fn attack_with_bow(&self, opponent: &mut Monster, distance: u32) {
+        if distance < 10 {
+            opponent.health -= 10;
+            println!(
+                "You attack with your bow. Your opponent has {} health left",
+                opponent.health
+            );
+        }
+    }
+
+    fn attack_with_rock(&self, opponent: &mut Monster, distance: u32) {
+        if distance < 3 {
+            opponent.health -= 4;
+            println!(
+                "You attack with your bow. Your opponent has {} health left",
+                opponent.health
+            );
+        }
+    }
+}
+
+impl FightFromDistance for Ranger {}
+
+fn main() {
+    let radagast = Wizard {};
+    let aragorn = Ranger {};
+
+    let mut uruk_hai = Monster {health: 40};
+
+    let distance = 8;
+    radagast.attack_with_sword(&mut uruk_hai);
+    aragorn.attack_with_bow(&mut uruk_hai, distance);
+}
