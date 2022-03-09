9c0ac313ce4223b9c1d485690b9fbdfc5d50ff7a
YoungHa Kim <ytok1108@aol.com>
2022-03-10 05:27:06 +0900

    Easy_rust_Eng.064__Trait_part4__001_result: This many characters: 68

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,16 @@ struct Cat {
 
 impl std::fmt::Display for Cat {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({}, {})", self.name, self.age)
+        let cat_type = match self.age {
+            0..=2 => "kitten",
+            3..=10 => "adult cat",
+            _ => "old cat",
+        };
+        write!(
+            f,
+            "{} is a cat who is {} years old, and therefore a {}",
+            self.name, self.age, cat_type
+        )
     }
 }
 
@@ -15,5 +24,8 @@ fn main() {
         name: "Reggie Mantle".to_string(),
         age: 4,
     };
-    println!("Reggie Mantle is a {:?}", mr_mantle);
+    println!(
+        "This many characters: {} ",
+        mr_mantle.to_string().chars().count()
+    );
 }
