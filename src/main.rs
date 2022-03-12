6594de8f58ba408de5ea034ea56fb6a2f5500783
YoungHa Kim <ytok1108@aol.com>
2022-03-12 14:45:09 +0900

    Traits part 7 _ understanding the From trait

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,23 @@
-use std::fmt::Debug;
+// Traits part 7 - understanding the From trait
 
-struct Monster {
-    health: i32,
-}
+// The From trait
+use std::fmt::Display;
 
-#[derive(Debug)]
-struct Wizard {
-    health: i32,
-}
-
-#[derive(Debug)]
-struct Ranger {
-    health: i32,
-}
-
-trait Magic {}
-trait FightClose {}
-trait FightFromDistance {} // POWERS
-
-impl FightClose for Ranger {}
-impl FightClose for Wizard {}
-impl FightFromDistance for Ranger {}
-impl Magic for Wizard {}
-
-fn attack_with_bow<T: FightFromDistance + Debug>(
-    character: &T,
-    opponent: &mut Monster,
-    distance: u32,
-) {
-    if distance < 10 {
-        opponent.health -= 10;
-        println!(
-            "You attack with your bow.
-        Your opponent now has {} health left.
-        You are now at: {:?}",
-            opponent.health, character
-        );
+fn print_vec<T: Display>(input: &Vec<T>) {
+    for item in input {
+        print!("{} ", item);
     }
+    println!();
 }
+fn main () {
 
-fn attack_with_sword<T: FightClose + Debug>(character: &T, opponent: &mut Monster) {
-    opponent.health -= 10;
-    println!(
-        "You attack with your sword.
-Your opponent now has {} health left. You are now at: {:?}",
-        opponent.health, character
-    );
-}
+    let array_vec = Vec::from([8,9,10]);
+    print_vec(&array_vec);
 
-fn fireball<T: Magic + Debug>(character: &T, opponent: &mut Monster, distance: u32) {
-    if distance < 15 {
-        opponent.health -= 20;
-        println!("You raise your hands and cast a fireball! Your opponent now has {} health left. You are now at: {:?}",
-        opponent.health, character);
-    }
-}
 
-fn main() {
-    let radagast = Wizard { health: 60 };
-    let aragorn = Ranger { health: 80 };
+    let str_vec = Vec::from("What kind of Vec will I be?");
+    print_vec(&str_vec);
 
-    let mut uruk_hai = Monster { health: 40 };
-
-    attack_with_bow(&aragorn, &mut uruk_hai, 8);
-    attack_with_sword(&radagast, &mut uruk_hai);
-    fireball(&radagast, &mut uruk_hai, 8)
+    let string_vec = Vec::from(String::from("What kind of Vec of Vec will I be then?"));
+    print_vec(&string_vec);
 }
