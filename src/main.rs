10f692c2628b8831d6f5e5079cf5960b573b21dd
YoungHa Kim <ytok1108@aol.com>
2022-03-29 06:38:40 +0900

    Using the dereference operator on a Box<i32>

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
-// Treating Smart Pointers Like Regular References with the "Deref" Trait
-
-// Note: there's one big difference between the MyBox<T> type we're about to build and the real Box<T>:our version will not store its data on the heap. We are focusing this example on "Deref", so where the data is actually stored is less important than the pointer-like behavior.
-
-
-// Following the Pointer to th Value with the Dereference Operator
-
+// Using "Box<T>" Like a Reference
 
 fn main () {
     let x = 5;
-    let y = &x;
+    let y = Box::new(x);
 
     assert_eq!(5, x);
     assert_eq!(5, *y);
