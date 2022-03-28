b16cb0763b5379e6939b50d47784ea8bfe62c92f
YoungHa Kim <ytok1108@aol.com>
2022-03-29 06:45:40 +0900

    Treating a Type Like a Reference by Implementing the __Deref

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
-// Using "Box<T>" Like a Reference
+// Treating a Type Like a Reference by Implementing the "Deref" Trait
+use std::ops::Deref;
+
+struct MyBox<T>(T);
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 fn main () {
     let x = 5;
-    let y = Box::new(x);
+    let y = MyBox(x);
 
     assert_eq!(5, x);
     assert_eq!(5, *y);
+    // *(y.deref())
 }
