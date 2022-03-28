140939afd734a56f11f032860af70e760118999d
YoungHa Kim <ytok1108@aol.com>
2022-03-29 06:25:38 +0900

    The first attempt at defining an enum to represent a cons list data structure of __i32__ values

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
-// Smart Pointers
-// A pointer is a general concept for a variable that contains an address in memory.
-// In Rust, the different smart pointers defined in the standard library provide functionality beyond that provided by references.
-// We've already encountered a few smart pointers in this book, such as "String" & "Vec<T>"in Chapter 8
+// The first attempt at defining an enum to represent a cons list data structure of "i32" values
 
-// We'll cover the most common smart pointers in the standard library:
-// "Box<T>" for allocating values on the heap
-// "Rc<T>", a reference counting type that enables multiple ownership
-// "Ref<T>" & "RefMut<T>", accessed through "RefCell<T>", a type that enforces the borrowing rules at runtime instead of compile time
+enum List {
+    Cons(i32, List)
+    Nil,
+}
 
-// Using "Box<T>" to Point to Data on the Heap
+use crate::List::{Cons, Nil};
+
 fn main() {
-    let b = Box::new(5);
-    println!("b = {b}");
+    let list = Cons(1, Cons(2, Cons(3, Nil)));
 }
