6053fba703773dac7a88d37ebaf66ca4bdf895cd
YoungHa Kim <ytok1108@aol.com>
2022-03-29 22:15:45 +0900

    Using __RefCell<T>__ to mutate an inner value while the outer value is considered immutable

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
-// Enforcing Borrowing Rules at Runtime with RefCell<T>
-// RefCell<T> and the Interior Mutability Pattern
-// Interior Mutability is a design pattern in Rust that allows you to mutate date even when there are immutable references to that data
-// Rc<T> enables multiple owners of the same data; Box<T> and RefCell<T> have single owners.
-// Box<T> allows immutable or mutable borrows checked at compile time; Rc<T> allows only immutable borrows checked at runtime.
-// Because RefCell<T> allows mutable borrows checked at runtime, you can mutate the value inside the RefCell<T> even when the RefCell<T> is immutable.
-
-// Interior Mutability: A Mutable Borrow to an Immutable Value.
+// A Use Case for Interior Mutability : Mock Objects
 
 
-fn main () {
-    let x =5;
-    let y = &mut x;
-}
+
+fn main () {}
