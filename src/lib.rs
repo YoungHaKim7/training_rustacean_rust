73c5676c7f791c7db43c263d2d9cc2bd2cea674f
YoungHa Kim <ytok1108@aol.com>
2022-03-29 22:23:29 +0900

    __self__ is a __&__ reference, so the data it refers to cannot be borrowed as mutable

--- src/lib.rs
+++ src/lib.rs
@@ -40,23 +40,22 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::cell::RefCell;
 
     struct MockMessenger {
-        sent_messages: RefCell<Vec<String>>,
+        sent_messages: Vec<String>,
     }
 
     impl MockMessenger {
         fn new() -> MockMessenger {
             MockMessenger {
-                sent_messages: RefCell::new(vec![]),
+                sent_messages: vec![],
             }
         }
     }
 
     impl Messenger for MockMessenger {
         fn send(&self, message: &str) {
-            self.sent_messages.borrow_mut().push(String::from(message));
+            self.sent_messages.push(String::from(message));
         }
     }
 
@@ -67,6 +66,6 @@ mod tests {
 
         limit_tracker.set_value(80);
 
-        assert_eq!(mock_messenger.sent_messages.borrow().len(), 1);
+        assert_eq!(mock_messenger.sent_messages.len(), 1);
     }
 }
