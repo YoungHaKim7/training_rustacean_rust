a45912ad1a8b7c61cb3d736e71908eea99c63cac
YoungHa Kim <ytok1108@aol.com>
2022-03-29 20:42:32 +0900

    Rust__Concurrency

---
+++ Python/py_vs_rust_concurrency.py
@@ -0,0 +1,3 @@
+messge = "Hello"
+for num in range(1, 100000, 1):
+    print("{} from task {}".format(messge, num))
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
-// Using "Rc<T>" to Share Data
-
-use crate::List::{Cons, Nil};
-use std::rc::Rc;
-
-enum List {
-    Cons(i32, Rc<List>),
-    Nil,
-}
-
-
+use std::thread;
 fn main() {
-    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
-    let b = Cons(3, Rc::clone(&a));
-    let c = Cons(4, Rc::clone(&a));
+    // 변수 message는 변경이 불가능하므로, 여러 개의 태스크에서 동시에 접근해도 안전하다.
+    let message = "Hello";
+    let mut threads = Vec::new();
+    // `for` 반복문은 `Iterator` trait 을 구현하는 어떤 객체에 대해서나 사용할 수 있다.
+    for num in 0..100000 {
+        // `thread::spawn` 을 통해 스레드를 생성한다.
+        threads.push(thread::spawn(move || {
+            println!("{message} form task {num:?}");
+        }));
+    }
+    // 각 스레드가 끝날때까지 기다린다.
+    for thread in threads {
+        thread.join().unwrap();
+    }
 }
+
