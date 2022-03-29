c330f130911367597196ac1abaf9db47cf93d5d9
YoungHa Kim <ytok1108@aol.com>
2022-03-30 07:44:20 +0900

    Using__Rc<RefCell<i32>>__ to create a __List__ that we can mutate result:a after = Cons(RefCell { value: 15 }, Nil) b after = Cons(RefCell { value: 3 }, Cons(RefCell { value: 15 }, Nil)) c after = Cons(RefCell { value: 4 }, Cons(RefCell { value: 15 }, Nil))

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
 // A Use Case for Interior Mutability : Mock Objects
+// Having multiple Owners of Mutable Data by Combining Rc<T> and RefCell<T>
+use crate::List::{Cons, Nil};
+use std::cell::RefCell;
+use std::rc::Rc;
 
+#[derive(Debug)]
+enum List {
+    Cons(Rc<RefCell<i32>>, Rc<List>),
+    Nil,
+}
 
+fn main() {
+    let value = Rc::new(RefCell::new(5));
 
-fn main () {}
+    let a = Rc::new(Cons(Rc::clone(&value), Rc::new(Nil)));
+
+    let b = Cons(Rc::new(RefCell::new(3)), Rc::clone(&a));
+    let c = Cons(Rc::new(RefCell::new(4)), Rc::clone(&a));
+
+    *value.borrow_mut() += 10;
+
+    println!("a after = {a:?}");
+    println!("b after = {b:?}");
+    println!("c after = {c:?}");
+}
