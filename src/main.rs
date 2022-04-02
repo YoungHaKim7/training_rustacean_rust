f8a761e786030c34ecb38f9655e58e8ba70e1c1f
YoungHa Kim <ytok1108@aol.com>
2022-04-02 15:41:32 +0900

    Saving a __JoinHandle__ for __thread::spawn__ to guarantee the thread is run to completion

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-// Creating a new thread to print one thing while the main thread prints something else
+// Saving a "JoinHandle" for "thread::spawn" to guarantee the thread is run to completion
 
 use std::time::Duration;
 use std::thread;
 
 fn main() {
-    thread::spawn(|| {
+    let handle = thread::spawn(|| {
         for i in 1..10 {
             println!("hi number {i} form the spawned thread!");
             thread::sleep(Duration::from_millis(1));
@@ -15,5 +15,7 @@ fn main() {
         println!("hi number {i}");
         thread::sleep(Duration::from_millis(1));
     }
+
+    handle.join().unwrap();
 }
 
