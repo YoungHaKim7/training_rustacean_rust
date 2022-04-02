b49e4d16b5229bf908da572a05ec77d6a661a4cc
YoungHa Kim <ytok1108@aol.com>
2022-04-02 17:09:36 +0900

    use of moved value: __counter__

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
-// Mutex is an abbreviation for mutual exclusion, an in,
-// a mutex allows only one thread to access some data at any given time.
+// Ten thread each increment a counter guarded by a __Mutex<T>__
 
-use std::sync::Mutex;
+use std::{sync::Mutex, thread};
+
 
 fn main() {
-    let m = Mutex::new(5);
-{
-        let mut num = m.lock().unwrap();
-        *num = 6;
+    let counter = Mutex::new(0);
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let handle = thread::spawn(move || {
+        let mut num = counter.lock().unwrap();
+
+        *num += 1;
+    });
+        handles.push(handle)
+    }
+    
+    for handle in handles {
+        handle.join().unwrap();
     }
 
-    println!("m = {m:?}");
-    
+    println!("Result: {}", *counter.lock().unwrap());
 }
