e3b9573547f71b2999bab57434b1a9d244a8d877
YoungHa Kim <ytok1108@aol.com>
2022-04-02 15:47:07 +0900

    __v__ is borrowed here

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,15 @@
+// Attempting to use a vector created by the main thread in another thread
 
-use std::time::Duration;
 use std::thread;
 
 fn main() {
+    let v = vec![1,2,3];
+
     let handle = thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {i} form the spawned thread!");
-            thread::sleep(Duration::from_millis(1));
+        println!("Here's a vector : {v:?}");
+    });
 
-    }});
     handle.join().unwrap();
-
-    for i in 1..5 {
-        println!("hi number {i}");
-        thread::sleep(Duration::from_millis(1));
-    }
 
 }
 
