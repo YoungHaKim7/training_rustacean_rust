6092f8e900281dafbcee898f7af81386dd90878c
YoungHa Kim <ytok1108@aol.com>
2022-04-09 22:36:32 +0900

    generics__error code ___dyn SomeCustomTrait__ doesn't implement __Debug__

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,21 @@
-// Constraint == Gernerics ___canot add 'T' to 'T'
-// fn dougs_func<T: std::ops::Add<Output = T> + std::ops::Sub<Output = T> + std::fmt::Debug>(
-//     input_a: T,
-//     input_b: T,
-// ) -> T {
-//     input_a - input_b
-// }
-
-#[allow(dead_code)]
-fn dougs_func2<T, E>(input_a: T, input_b: T, input_e: E) -> T
-where
-    T: std::ops::Add<Output = T> + std::ops::Sub<Output = T> + std::fmt::Debug,
-    E: std::fmt::Debug,
-{
-    println!("input_a has {:?}", input_a);
-    println!("input_e has {:?}", input_e);
-    input_a - input_b
+trait SomeCustomTrait {
+    fn blah_blah(&self, a: &str, b: &str) -> String;
 }
 
-// Not about programming to Specific Type
-// It's about programming to CAPABILITIES of Types via Constrains on Traits
+
+#[allow(debug_code)]
+fn do_this<T>(some_var: &T) -> String
+where T: SomeCustomTrait {
+    // Some complex logic
+    some_var.blah_blah("first", "second")
+}
+
+fn do_this2(some_var: &dyn SomeCustomTrait) -> String {
+    // Some complex logic..
+    println!("{:?}", some_var);
+    some_var.blah_blah("first", "second")
+}
 
 fn main() {
-    let a = dougs_func2(4.4, 5.5, "debug".to_string());
-    println!("a has {a}");
+    
 }
