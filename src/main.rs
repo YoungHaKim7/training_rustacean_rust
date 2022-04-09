fe6b659297aba974fa14139083fa807937982436
YoungHa Kim <ytok1108@aol.com>
2022-04-09 22:07:39 +0900

    Generics___func_(T)__good

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,8 @@
-#[derive(Debug)]
-enum SomeEnum<T> {
-    OptionA(T),
-    OptionB(T),
-    OptionC,
+fn dougs_func<T>(input_a: T, input_b: T) -> T {
+    input_a
 }
 
 fn main() {
-    let some_data = SomeEnum::OptionA(34.2);
-
-    match some_data {
-        SomeEnum::OptionA(a) => println!("OptionA contains {}", a),
-        SomeEnum::OptionB(b) => println!("OptionB contains {}", b),
-        SomeEnum::OptionC => println!("Boring option c : "),
-    }
-
-    let some_data2 = SomeEnum::OptionB('b');
-    println!("{some_data2:?}");
-    let some_data3 = SomeEnum::OptionA(vec![1, 2, 3]);
-    println!("{some_data3:?}");
+    let a = dougs_func(4 as i8, 5);
+    println!("a has {a}")
 }
