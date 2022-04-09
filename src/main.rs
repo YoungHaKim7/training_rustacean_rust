4c1c4e0d30971a29be8bf12d4ea806b77138ff5f
YoungHa Kim <ytok1108@aol.com>
2022-04-09 21:38:02 +0900

    Rust T was just a placeholder

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,20 @@
-struct PointI32 {
-    x: i32,
-    y: i32,
-}
-struct PointF64 {
-    x: f64,
-    y: f64,
+// struct PointI32 {
+//     x: i32,
+//     y: i32,
+// }
+// struct PointF64 {
+//     x: f64,
+//     y: f64,
+// }
+struct Point<T> {
+    x: T,
+    y: T,
 }
 
 fn main() {
-    let a = PointI32 { x: 100, y: -1};
+    let a = Point { x: 100, y: -1 };
     println!("x = {}  y = {}", a.x, a.y);
+
+    let b = Point { x: 10.1, y: -2.3 };
+    println!("x = {}  y = {}", b.x, b.y);
 }
