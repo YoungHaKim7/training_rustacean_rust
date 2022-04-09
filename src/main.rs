238e5d01c27a6e6e5c7dee4a1b96d1c8ae216da4
YoungHa Kim <ytok1108@aol.com>
2022-04-09 22:43:20 +0900

    generics__i32__impl____do this

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,36 @@ trait SomeCustomTrait {
     fn blah_blah(&self, a: &str, b: &str) -> String;
 }
 
+#[derive(Debug)]
+struct DougsStruct {
+    something: i32,
+}
+
+impl SomeCustomTrait for DougsStruct {
+    fn blah_blah(&self, a: &str, b: &str) -> String {
+        self.something.to_string() + " - " + a + " - " + b
+    }
+}
 
 #[allow(debug_code)]
 fn do_this<T>(some_var: &T) -> String
-where T: SomeCustomTrait {
+where
+    T: SomeCustomTrait,
+{
     // Some complex logic
     some_var.blah_blah("first", "second")
 }
 
-fn do_this2(some_var: &dyn SomeCustomTrait) -> String {
-    // Some complex logic..
-    println!("{:?}", some_var);
-    some_var.blah_blah("first", "second")
+impl SomeCustomTrait for i32 {
+    fn blah_blah(&self, a: &str, b: &str) -> String {
+        "i32".to_string() + " - " + a + " - " + b
+    }
 }
 
 fn main() {
-    
+    let test = DougsStruct { something: 1000 };
+    let result = do_this(&test);
+
+    let testi32 = 18;
+    let result = do_this(&testi32);
 }
