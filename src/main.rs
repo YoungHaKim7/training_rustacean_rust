33a08beef1517c014a3e7ebb39444bfcd61f2ad1
YoungHa Kim <ytok1108@aol.com>
2022-04-09 22:25:57 +0900

    Not about programming to Specific Type ___It's about programming to CAPABILITIES of Types via Constrains on Traits

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
 // Constraint == Gernerics ___canot add 'T' to 'T'
-fn dougs_func<T: std::ops::Add<Output = T> + std::ops::Sub<Output = T> + std::fmt::Debug>(
-    input_a: T,
-    input_b: T,
-) -> T {
+// fn dougs_func<T: std::ops::Add<Output = T> + std::ops::Sub<Output = T> + std::fmt::Debug>(
+//     input_a: T,
+//     input_b: T,
+// ) -> T {
+//     input_a - input_b
+// }
+
+#[allow(dead_code)]
+fn dougs_func2<T, E>(input_a: T, input_b: T, input_e: E) -> T
+where
+    T: std::ops::Add<Output = T> + std::ops::Sub<Output = T> + std::fmt::Debug,
+    E: std::fmt::Debug,
+{
+    println!("input_a has {:?}", input_a);
+    println!("input_e has {:?}", input_e);
     input_a - input_b
 }
 
+// Not about programming to Specific Type
+// It's about programming to CAPABILITIES of Types via Constrains on Traits
+
 fn main() {
-    let a = dougs_func(4.4, 5.5);
-    println!("a has {a}")
+    let a = dougs_func2(4.4, 5.5);
+    println!("a has {a}");
 }
