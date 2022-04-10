1661f72c15807902eae601d1ad7253e1d243937f
YoungHa Kim <ytok1108@aol.com>
2022-04-10 23:19:45 +0900

    Generics __trait__sum2__

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,24 @@ impl MyTrait for MyStruct {
     }
 }
 
+struct MyStruct2 {
+    size: u32,
+}
+
+impl MyTrait for MyStruct2 {
+    fn sum(&self) -> u32 {
+        self.size
+    }
+}
+
+fn print_sum(m: &impl MyTrait) {
+    println!("sum: {}", m.sum());
+}
+
 fn main() {
     let my_struct = MyStruct {};
+    let my_struct2 = MyStruct2 { size: 12 };
 
-    println!("sum: {}", my_struct.sum());
+    print_sum(&my_struct);
+    print_sum(&my_struct2);
 }
