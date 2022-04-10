7de480896455b6df0d584b931bdd0b7c149ebec1
YoungHa Kim <ytok1108@aol.com>
2022-04-10 23:21:58 +0900

    Generics __trait__default

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
+//default
 pub trait MyTrait {
-    fn sum(&self) -> u32;
-}
-
-struct MyStruct {}
-impl MyTrait for MyStruct {
     fn sum(&self) -> u32 {
         10
     }
 }
+
+struct MyStruct {}
+impl MyTrait for MyStruct {}
 
 struct MyStruct2 {
     size: u32,
@@ -25,8 +24,6 @@ fn print_sum(m: &impl MyTrait) {
 
 fn main() {
     let my_struct = MyStruct {};
-    let my_struct2 = MyStruct2 { size: 12 };
 
     print_sum(&my_struct);
-    print_sum(&my_struct2);
 }
