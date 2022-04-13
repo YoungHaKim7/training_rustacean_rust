72583548d554fc0b903be07699bb018fc014961c
YoungHa Kim <ytok1108@aol.com>
2022-04-14 00:30:32 +0900

    & immutable reference vs &mut mutable reference(unique reference)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
+// & immutable reference / shared reference
+// &mut mutable reference / unique reference
+// & *
+
 fn main() {
     let mut my_number = 9;
     let num_ref = &mut my_number;
 
     *num_ref = 10;
+    // &mut mutable reference / unique reference
 
     println!("Number is now {my_number}");
 }
