72bf087238997cca231db9ffdaa3003abcfee851
YoungHa Kim <ytok1108@aol.com>
2022-04-18 01:41:36 +0900

    stack u8__string print__result: 236 149 136 235 133 149 32 안녕

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,10 @@
-// use std::str;
+use std::str;
 
-// fn main() {
-//     for b in "안녕 ".bytes() {
-//         println!("{}", b);
-//     }
-//     let a = str::from_utf8(&a).unwrap();
-//     println!("{a:?}");
-//     let mut my_vec = Vec::new();
-//     for my_vec in "하세요 저는 영 ".bytes() {
-//         vec.append(my_vec);
-//     }
-//     let my_vec = str::from_utf8(&my_vec).unwrap();
-//     println!("{my_vec}");
-// }
 fn main() {
-    let mut vec = vec![1, 2, 3];
-    let mut my_vec2 = vec![236, 149, 136, 235, 133, 149, 32];
-    vec.append(&mut my_vec2);
-    println!("{vec:?}");
+    for b in "안녕 ".bytes() {
+        println!("{}", b);
+    }
+    let a = vec![236, 149, 136, 235, 133, 149, 32];
+    let a = str::from_utf8(&a).unwrap();
+    println!("{a}");
 }
