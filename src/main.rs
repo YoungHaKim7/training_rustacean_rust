fee381cfef1372b2238c0f64b39cb0b66b113d67
YoungHa Kim <ytok1108@aol.com>
2022-04-17 23:31:43 +0900

    spark heart print

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,11 @@
+use std::str;
 
 fn main() {
-    let s = String::from("hello");
-    println!("{s}");
-    let t = s.to_string();
-    let t = " and good world!".to_string();
-    println!("{t}");
+    // some bytes, in a vector
+    let sparkle_heart = vec![240, 159, 146, 150];
 
-    let data = "initial contents good";
+    // We know these bytes are valid, so just use `unwrap()`.
+    let sparkle_heart = str::from_utf8(&sparkle_heart).unwrap();
 
-    let z = data.to_string();
-
-    // the method also works on a literal directly:
-    let z = "initial contents".to_string();
-    println!("{z}");
-    let a = String::from("Sting initial contents");
-    let x = a.to_string();
-
-    // the method also works on a literal directly:
-    let x = "initial contents".to_string();
-    println!("{x}");
-
-    let hello1 = String::from("السلام عليكم");
-    let hello = String::from("Dobrý den");
-    let hello = String::from("Hello");
-    let hello = String::from("שָׁלוֹם");
-    let hello = String::from("नमस्ते");
-    let hello = String::from("こんにちは");
-    let hello = String::from("안녕하세요");
-    let hello = String::from("你好");
-    let hello = String::from("Olá");
-    let hello = String::from("Здравствуйте");
-    let hello = String::from("Hola");
-    println!("{hello1}");
-
-    let len = String::from("Hola").len();
-    println!("{len}");
-    let len2 = String::from("Здравствуйте").len();
-    println!("{len2}");
-
-
-
+    println!("{}", sparkle_heart);
 }
