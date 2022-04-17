8f50d4e3e995515cf1c0a178861389c3e2507231
YoungHa Kim <ytok1108@aol.com>
2022-04-18 01:32:26 +0900

    vec__append

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
-use std::str;
+// use std::str;
 
+// fn main() {
+//     for b in "안녕 ".bytes() {
+//         println!("{}", b);
+//     }
+//     let a = str::from_utf8(&a).unwrap();
+//     println!("{a:?}");
+//     let mut my_vec = Vec::new();
+//     for my_vec in "하세요 저는 영 ".bytes() {
+//         vec.append(my_vec);
+//     }
+//     let my_vec = str::from_utf8(&my_vec).unwrap();
+//     println!("{my_vec}");
+// }
 fn main() {
-    // some bytes, in a vector
-    let sparkle_heart = vec![240, 159, 146, 150];
-
-    // We know these bytes are valid, so just use `unwrap()`.
-    let sparkle_heart = str::from_utf8(&sparkle_heart).unwrap();
-
-    println!("{}", sparkle_heart);
+    let mut vec = vec![1, 2, 3];
+    let mut my_vec2 = vec![236, 149, 136, 235, 133, 149, 32];
+    vec.append(&mut my_vec2);
+    println!("{vec:?}");
 }
