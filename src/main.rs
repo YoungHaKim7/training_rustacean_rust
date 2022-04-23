46c476f1a11a861cd96f83870c0afe820c054b1b
YoungHa Kim <ytok1108@aol.com>
2022-04-23 19:55:10 +0900

    timsort ____TheAlgorithms_Rust

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,6 @@
+
+
+
 fn main() {
-    let mut my_name = String::with_capacity(30);
-    my_name.push_str("String::from, hi Kim");
-    println!(
-        "Capacity is : {}, lens is : {}",
-        my_name.capacity(),
-        my_name.len()
-    );
-    my_name.push_str("String::from, gogogogogo");
-    println!(
-        "Capacity is : {}, lens is : {}",
-        my_name.capacity(),
-        my_name.len()
-    );
-    my_name.push_str("String::from, gogogogogo");
-
-    println!(
-        "Capacity is : {}, lens is : {}",
-        my_name.capacity(),
-        my_name.len()
-    );
-    my_name
-        .push_str("String::from, gogogogogogogogogogogogogogogogogogogogogogogogogogogogogooggo");
-
-    println!(
-        "Capacity is : {}, lens is : {}",
-        my_name.capacity(),
-        my_name.len()
-    );
-}
+    
+} 
