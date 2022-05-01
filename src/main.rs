78dcf4b80920714690a3e517f8b2515706249e42
YoungHa Kim <ytok1108@aol.com>
2022-05-01 21:06:09 +0900

    Rust code+______Borrowing_result: Hello, Young ! Hello again, Young

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-fn greet(s: String) {
-    println!("Hello, {s} !");
+fn greet(s: &String) {
+    println!("Hello, {} !", s);
 }
 
 fn main() {
-    let name = String::from("Pavel");
-    greet(name);
-    println!("Hello again, {name}");
+    let name = String::from("Young");
+    greet(&name);
+    println!("Hello again, {}", name);
 }
 
