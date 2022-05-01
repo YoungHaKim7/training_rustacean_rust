5c0698c4001ac80a967e8ee7aafa75fd67b1a490
YoungHa Kim <ytok1108@aol.com>
2022-05-01 20:59:35 +0900

    rust__clone __vector

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let v1 = vec![1, 2, 3];
-    let v2 = v1;
-    let v3 = v1;
+    let v2 = v1.clone();
+    let v3 = v1.clone();
 
     println!("{} {} {}", v1.len(), v2.len(), v3.len());
 }
