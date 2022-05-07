da6f549d706d176a3b158c855c9236a1f430544d
YoungHa Kim <ytok1108@aol.com>
2022-05-07 11:23:13 +0900

    Const__ex ___result: Area of the circle: 160.20200192305325 cm2

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-
+const PI: f64 = 3.1415926535;
 
 fn main() {
-    static mut FOO: [i32; 5] = [1, 2, 3, 4, 5];
-
-    unsafe {
-        println!("{FOO:?}")
-    }
-    
+    let radius:f64 = 7.141;
+    println!("Area of the circle: {} cm2", PI*radius*radius);
 }
