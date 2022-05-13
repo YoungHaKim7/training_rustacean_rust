4d030e5fb27face7e63ce87d16531416afee3adc
YoungHa Kim <ytok1108@aol.com>
2022-05-13 20:58:44 +0900

    vector_cpp__input & display__

--- Cpp_Lang/vector_stl_.cpp
+++ Cpp_Lang/vector_stl_.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+void display(vector<int> &v) {
+  for (int i = 0; i < v.size(); i++) {
+    cout << v[i] << " ";
+  }
+  cout << endl;
+}
+
 int main() {
   vector<int> vec1;
   int element;
@@ -11,6 +18,7 @@ int main() {
     cin >> element;
     vec1.push_back(element);
   }
+  display(vec1);
 
   return 0;
 }
