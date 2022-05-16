4147448a4af45fe8c9f2fe7c07715ded693a81cb
YoungHa Kim <ytok1108@aol.com>
2022-05-16 21:32:56 +0900

    C++ largely wiill not care about the template or how it might be used.

--- Cpp_Lang/generic.cpp
+++ Cpp_Lang/generic.cpp
@@ -1,16 +1,8 @@
 #include <iostream>
 
-using namespace std;
-template <class T, class U> class A {
-  T x;
-  U y;
-
-public:
-  A() { cout << "Constructor Called" << endl; }
-};
+template <typename T> T timestwo(T number) { return number + number; }
 
 int main() {
-  A<char, char> a;
-  A<int, double> b;
+  std::cout << "Hello, world\n";
   return 0;
 }
