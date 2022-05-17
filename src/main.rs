603458acc3aeb6be4b6bb8e6548e5ba183e476a0
YoungHa Kim <ytok1108@aol.com>
2022-05-18 00:50:42 +0900

    rust 0o204__132___contains_print

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,8 @@ fn main() {
     let needle = 0o204;
     let haystack = vec![1, 1, 2, 5, 14, 132, 420, 1430];
 
-    for item in &haystack {
-        if *item == needle {
-            println!("{item}");
-        }
+    if haystack.contains(&needle) {
+        println!("{needle}");
     }
 }
 
