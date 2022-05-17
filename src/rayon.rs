539ef595f89a87aa15804fa496c3ba3ce0b18536
YoungHa Kim <ytok1108@aol.com>
2022-05-17 21:23:13 +0900

    parallel any/all.

--- src/rayon.rs
+++ src/rayon.rs
@@ -1,18 +1,11 @@
 use rayon::prelude::*;
 
 fn main() {
-    // parallel foreach .
     let mut arr = [0, 7, 9, 11];
     arr.par_iter_mut().for_each(|p| {
         let id = std::thread::current().id();
         println!("thread id : {id:?}");
         *p += 1;
     });
-
-    // parallel any/all.
-    assert!(arr.par_iter().any(|n| (*n % 2) != 0)); //don't throw panic
-    assert!(arr.par_iter().all(|n| (*n >= 1))); //don't throw panic
-
     println!("rayon let's go !!!");
 }
-
