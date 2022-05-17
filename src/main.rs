9bdd39a05e63993fbb9696d15b938eea4f1021fc
YoungHa Kim <ytok1108@aol.com>
2022-05-18 00:48:58 +0900

    rust 0o204__132____print

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
-use rayon::prelude::*;
-
 fn main() {
-    // parallel foreach .
-    let mut arr = [0, 7, 9, 11];
-    arr.par_iter_mut().for_each(|p| {
-        let id = std::thread::current().id();
-        println!("thread id : {id:?}");
-        *p += 1;
-    });
+    let needle = 0o204;
+    let haystack = vec![1, 1, 2, 5, 14, 132, 420, 1430];
 
-    // parallel any/all.
-    assert!(arr.par_iter().any(|n| (*n % 2) != 0)); //don't throw panic
-    assert!(arr.par_iter().all(|n| (*n >= 1))); //don't throw panic
-
-    println!("rayon let's go !!!");
+    for item in &haystack {
+        if *item == needle {
+            println!("{item}");
+        }
+    }
 }
 
